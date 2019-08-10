2e268ac00e2bfd5a5a834af3a1f8e80f8f134e9c
macintosh HD <macintoshHD@MacBook-Pro-cua-Q-Mac.local>
2019-08-10 17:35:59 +0700

    commit 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { TimelineComponent } from './home/timeline/timeline.component';
+import { SkillComponent } from './home/skill/skill.component';
+import { ExperienceComponent } from './home/experience/experience.component';
+import { InterestComponent } from './home/interest/interest.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+  {
+    path: 'education',
+    component: TimelineComponent
+  },
+  {
+    path: 'skills',
+    component: SkillComponent
+  },
+  {
+    path: 'experience',
+    component: ExperienceComponent
+  },
+  {
+    path: 'interest',
+    component: InterestComponent
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true,
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
